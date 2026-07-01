ecaa251064d2db4bb4d4ec2d4da8c05a115a90b3
saidpadillatapia <padillsaid39@email.com>
2026-07-01 13:57:17 -0600

    acelsdsadafdsfea

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -256,7 +256,7 @@
             </a>
             <a href="{{ route('admin.alta-producto-pt') }}" class="sb-link {{ request()->is('admin/alta-producto-pt*') ? 'active' : '' }}">
                 <div class="sb-icon"><svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="#6b7280" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M20 7l-8-4-8 4m16 0l-8 4m8-4v10l-8 4m0-10L4 7m8 4v10M4 7v10l8 4"/></svg></div>
-                <span class="sb-text">Alta Producto Comercial PT</span>
+                <span class="sb-text">Alta Producto Comercial</span>
             </a>
 
             <div class="sb-hr"></div>
